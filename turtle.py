3424792ce3ac54136eb16823152f5c7d523ac989
EcZey <150579297+EcZey@users.noreply.github.com>
2023-11-18 11:15:53 +0300

    turtle

---
+++ turtle.py
@@ -0,0 +1,15 @@
+turtle.goto(100,-200)
+
+def ciz(kenar,buyukluk):
+    for a in range(kenar):
+        turtle.forward(buyukluk)
+        turtle.right(360/kenar)
+
+for a in range(5):
+    turtle.penup()
+    xx=random.randint(-200,200)
+    yy=random.randint(-200,200)
+    turtle.goto(xx,yy)
+    turtle.pendown()
+    b=random.randint(50,200)
+    ciz (random.randint(3,7),b)
